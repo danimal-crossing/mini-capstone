f3f03a48cd7748af252874b8ebf975f7fa5605f8
dani <dani.zaghian@gmail.com>
2020-05-27 12:02:23 -0700

    adds unique urls to see different products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,4 +5,14 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jb"
   end
 
+  def one_product_action
+    @product = Product.first
+    render "one_product.json.jb"
+  end
+
+  def second_product_action
+    @product = Product.second
+    render "one_product.json.jb"
+  end
+
 end
